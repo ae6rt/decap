5170552372a57a59b79710db03f0acf994b67a8c
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-02 20:44:25 -0700

    change application user to decap

--- aws-resources/create-buckets.sh
+++ aws-resources/create-buckets.sh
@@ -12,5 +12,5 @@ REGION="us-west-1"
 echo "===Creating buckets" 
 
 for i in console-logs build-artifacts; do 
-	aws --profile $AWS_PROFILE s3api create-bucket --bucket ${BUCKET_NAME_PREFIX}-$i --create-bucket-configuration LocationConstraint=$REGION
+	aws --profile $AWS_PROFILE s3api create-bucket --bucket ${BUCKET_NAME_PREFIX}-$i --create-bucket-configuration LocationConstraint=$REGION >> aws.log
 done
