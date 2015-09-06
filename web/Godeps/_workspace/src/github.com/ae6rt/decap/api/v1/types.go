ad8c2aa5b2e930bf78c1c200462b7b943aae5449
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-05 17:43:16 -0700

    checkpoint

---
+++ web/Godeps/_workspace/src/github.com/ae6rt/decap/api/v1/types.go
@@ -0,0 +1,17 @@
+package v1
+
+type Version struct {
+	Version string `json:"version"`
+}
+
+type Project struct {
+	Key string `json:"key"`
+}
+
+type Build struct {
+	ID       string `json:"id"`
+	Branch   string `json:"branch"`
+	Duration int64  `json:"duration"`
+	Result   int    `json:"result"`
+	UnixTime int64  `json:"unixtime"`
+}
