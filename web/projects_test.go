8d289f90a1112546fb267ae3f3e2a7e4f1696b97
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-07 19:33:12 -0700

    add projects_test

---
+++ web/projects_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ae6rt/ziptools"
+)
+
+func TestProjects(t *testing.T) {
+	dir, err := ziptools.Unzip("buildscripts-repo.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	proj, err := findProjects("file://"+dir, "master")
+	os.RemoveAll(dir)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(proj) != 3 {
+		t.Fatal(err)
+	}
+
+	foundIt := false
+	for _, v := range proj {
+		if v.Parent == "ae6rt" && v.Library == "dynamodb-lab" {
+			foundIt = true
+			if v.Descriptor.RepoManager != "github" {
+				t.Fatalf("Want github but got %s\n", v.Descriptor.RepoManager)
+			}
+			if v.Descriptor.RepoURL != "https://github.com/ae6rt/dynamodb-lab.git" {
+				t.Fatalf("Want https://github.com/ae6rt/dynamodb-lab.git but got %s\n", v.Descriptor.RepoManager)
+			}
+			break
+		}
+	}
+	if !foundIt {
+		t.Fatalf("Want a project ae6rt/library but did not find one\n")
+	}
+}
