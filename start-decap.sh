dc826be9797fd23c5075923bf2f5d2e2a501f73b
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-09 08:50:55 -0700

    update start-decap.sh

--- start-decap.sh
+++ start-decap.sh
@@ -2,5 +2,7 @@
 
 set -ux
 
-kubectl create -f k8s-resources/aws-secret.yaml
+kubectl create -f k8s-resources/decap-namespaces.yaml
+kubectl --namespace=decap create -f k8s-resources/decap-secrets.yaml
+kubectl --namespace=decap-system create -f k8s-resources/decap-secrets.yaml
 kubectl create -f k8s-resources/decap.yaml 
