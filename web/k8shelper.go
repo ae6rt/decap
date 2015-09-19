5ed835d14a6103aec8bb316302b17b2a71e9fb9b
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-19 09:55:28 -0700

    add comments about Github event types

--- web/k8shelper.go
+++ web/k8shelper.go
@@ -49,6 +49,10 @@ type Terminated struct {
 	ExitCode    int    `json:"exitCode"`
 }
 
+// TODO distinguish between pushes and branch creation.  Github has a header value that allows these to be differentitated.
+// https://developer.github.com/webhooks/#delivery-headers
+// https://gist.githubusercontent.com/ae6rt/53a25e726ac00b4cb535/raw/e3f412f6e7f408a56d0d691a1ec8b7658a495124/gh-create.json
+// https://gist.githubusercontent.com/ae6rt/2be93f7d5edef8030b52/raw/29f591eb8ecc5555c55f1878b545613c1f9839b7/gh-push.json
 type BuildEvent interface {
 	Team() string
 	Library() string
