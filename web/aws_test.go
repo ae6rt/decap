2c2829b364c1014b75e0ffd214c9ea3ff99ca5e1
Mark Petrovic <mark.petrovic@xoom.com>
2015-09-21 17:08:35 -0700

    round out test of get-buckets

--- web/aws_test.go
+++ web/aws_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,9 +9,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
-func TestAWSS3Get(t *testing.T) {
+func TestAWSS3GetArtifacts(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
+		if r.URL.Path != "/decap-build-artifacts/buildID" {
+			t.Fatalf("Want /decap-build-artifacts/buildID but got %s\n", r.URL.Path)
+		}
+		w.Write([]byte{0})
 	}))
 	defer testServer.Close()
 
@@ -23,6 +25,34 @@ func TestAWSS3Get(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(data)
+	if len(data) != 1 {
+		t.Fatalf("Want 1 but got %d\n", len(data))
+	}
+	if data[0] != 0 {
+		t.Fatalf("Want 0 but got %d\n", data[0])
+	}
+}
 
+func TestAWSS3GetConsoleLogs(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/decap-console-logs/buildID" {
+			t.Fatalf("Want /decap-console-logs/buildID but got %s\n", r.URL.Path)
+		}
+		w.Write([]byte{0})
+	}))
+	defer testServer.Close()
+
+	config := aws.NewConfig().WithCredentials(credentials.NewStaticCredentials("key", "secret", "")).WithRegion("region").WithMaxRetries(3).WithEndpoint(testServer.URL).WithS3ForcePathStyle(true)
+
+	c := AWSStorageService{config}
+	data, err := c.GetConsoleLog("buildID")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(data) != 1 {
+		t.Fatalf("Want 1 but got %d\n", len(data))
+	}
+	if data[0] != 0 {
+		t.Fatalf("Want 0 but got %d\n", data[0])
+	}
 }
