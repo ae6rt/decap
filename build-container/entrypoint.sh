5d8edfc43b4522b5371c6c2839ebd08834fc22a2
Mark Petrovic <mark.petrovic@xoom.com>
2015-10-02 18:47:07 -0700

    fix typo in entrypoint.sh script

--- build-container/entrypoint.sh
+++ build-container/entrypoint.sh
@@ -35,7 +35,7 @@ if [ $# -eq 0 ]; then
 
    bctool record-build-metadata  --aws-access-key-id ${AWS_ACCESS_KEY_ID} --aws-secret-access-key ${AWS_ACCESS_SECRET_ID} --aws-region ${AWS_DEFAULT_REGION} \
 	--table-name decap-build-metadata  --build-id ${BUILD_ID}  --project-key ${PROJECT_KEY} --branch ${BRANCH_TO_BUILD} \
-	--build-start-time ${START} --build-duration ${DURATION} --build-result ${BUILD_EXIT_CODE} 
+	--build-start-time ${START} --build-duration ${DURATION} --build-result ${BUILD_EXITCODE} 
 
 else
    exec "$@"
