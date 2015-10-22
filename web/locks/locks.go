657db50e6c556fe0c23fa0c2e4d49033708bea18
Mark Petrovic <mark.petrovic@xoom.com>
2015-10-21 19:55:22 -0700

    add comments to lock package public members

--- web/locks/locks.go
+++ web/locks/locks.go
@@ -9,9 +9,10 @@ import (
 	"golang.org/x/net/context"
 )
 
-const BUILDLOCKS = "/buildlocks"
-const DEFERRED = "/deferred"
+const buildLocks = "/buildlocks"
+const deferred = "/deferred"
 
+// Locker defines the interface the build locker and build deferral instance makes available.
 type Locker interface {
 	Lock(key, value string) (*etcd.Response, error)
 	Unlock(key, value string) (*etcd.Response, error)
@@ -22,31 +23,37 @@ type Locker interface {
 	Key(projectKey, branch string) string
 }
 
+// Deferral models a deferred build.  Data is the serialized build event, while key is the identifier assigned to this deferred
+// build by the locker service.
 type Deferral struct {
 	Data string `json:"-"`
 	Key  string `json:"key"`
 }
 
+// EtcdLocker is the Locker implementation on top of etcd.
 type EtcdLocker struct {
 	Config etcd.Config
 }
 
+// Lock locks a build on top of etcd.
 func (d EtcdLocker) Lock(key, value string) (*etcd.Response, error) {
 	c, err := etcd.New(d.Config)
 	if err != nil {
 		return nil, err
 	}
-	return etcd.NewKeysAPI(c).Set(context.Background(), BUILDLOCKS+"/"+key, value, &etcd.SetOptions{PrevExist: etcd.PrevNoExist})
+	return etcd.NewKeysAPI(c).Set(context.Background(), buildLocks+"/"+key, value, &etcd.SetOptions{PrevExist: etcd.PrevNoExist})
 }
 
+// Unlock unlocks a build on top of etcd.
 func (d EtcdLocker) Unlock(key, value string) (*etcd.Response, error) {
 	c, err := etcd.New(d.Config)
 	if err != nil {
 		return nil, err
 	}
-	return etcd.NewKeysAPI(c).Delete(context.Background(), BUILDLOCKS+"/"+key, &etcd.DeleteOptions{PrevValue: value})
+	return etcd.NewKeysAPI(c).Delete(context.Background(), buildLocks+"/"+key, &etcd.DeleteOptions{PrevValue: value})
 }
 
+// Key defines an unique, opaque key that locks a build.
 func (d EtcdLocker) Key(projectKey, branch string) string {
 	return hex.EncodeToString([]byte(fmt.Sprintf("%s/%s", projectKey, branch)))
 }
@@ -56,9 +63,10 @@ func (d EtcdLocker) Defer(buildEvent []byte) (*etcd.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	return etcd.NewKeysAPI(c).CreateInOrder(context.Background(), DEFERRED, string(buildEvent), nil)
+	return etcd.NewKeysAPI(c).CreateInOrder(context.Background(), deferred, string(buildEvent), nil)
 }
 
+// ClearDeferred removes a build from the deferred list.
 func (d EtcdLocker) ClearDeferred(deferralID string) (*etcd.Response, error) {
 	c, err := etcd.New(d.Config)
 	if err != nil {
@@ -67,12 +75,13 @@ func (d EtcdLocker) ClearDeferred(deferralID string) (*etcd.Response, error) {
 	return etcd.NewKeysAPI(c).Delete(context.Background(), deferralID, nil)
 }
 
+// DeferredBuilds returns the list of builds currently deferred.
 func (d EtcdLocker) DeferredBuilds() ([]Deferral, error) {
 	c, err := etcd.New(d.Config)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := etcd.NewKeysAPI(c).Get(context.Background(), DEFERRED, &etcd.GetOptions{Recursive: true, Sort: true})
+	resp, err := etcd.NewKeysAPI(c).Get(context.Background(), deferred, &etcd.GetOptions{Recursive: true, Sort: true})
 	if err != nil {
 		return nil, err
 	}
@@ -85,7 +94,7 @@ func (d EtcdLocker) DeferredBuilds() ([]Deferral, error) {
 	return events, nil
 }
 
-// Create the deferred directory in etcd.  Check whether a listing is available; if one is, do not create the directory again.
+// InitDeferred creates the directory in etcd where deferred builds are stored.
 func (d EtcdLocker) InitDeferred() error {
 	_, err := d.DeferredBuilds()
 	if err != nil {
@@ -94,11 +103,12 @@ func (d EtcdLocker) InitDeferred() error {
 		if err != nil {
 			return err
 		}
-		_, err = etcd.NewKeysAPI(c).Set(context.Background(), DEFERRED, "", &etcd.SetOptions{Dir: true})
+		_, err = etcd.NewKeysAPI(c).Set(context.Background(), deferred, "", &etcd.SetOptions{Dir: true})
 	}
 	return err
 }
 
+// New returns a new etcd implementation of a Locker.
 func NewEtcdLocker(machines []string) Locker {
 	return EtcdLocker{Config: etcd.Config{
 		Endpoints: machines,
