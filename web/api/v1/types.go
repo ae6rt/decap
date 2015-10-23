ba41c8f578d7599b2aa5e73266ecba26a0fc217e
Mark Petrovic <mark.petrovic@xoom.com>
2015-10-23 07:34:27 -0700

    add some godocs

--- web/api/v1/types.go
+++ web/api/v1/types.go
@@ -6,10 +6,12 @@ import (
 	"github.com/ae6rt/decap/web/locks"
 )
 
+// Meta models fields common to most publicly exposed types.
 type Meta struct {
 	Error string `json:"error,omitempty"`
 }
 
+// Version models the version of Decap
 type Version struct {
 	Meta
 	Version string `json:"version"`
@@ -18,11 +20,15 @@ type Version struct {
 	SDK     string `json:"sdk"`
 }
 
+// Projects models the collection of projects managed by Decap
 type Projects struct {
 	Meta
 	Projects []Project `json:"projects"`
 }
 
+// Project models a specific project.  Sidecars are literal Kubernetes container specs articulated in JSON.  A user can
+// include any amount of detail in the side car spec, which will be deserialized into a native Kubernetes container type
+// for inclusion in the materialized build pod.
 type Project struct {
 	Team        string            `json:"team"`
 	ProjectName string            `json:"project"`
@@ -30,24 +36,35 @@ type Project struct {
 	Sidecars    []string          `json:"sidecars,omitempty"`
 }
 
+// ProjectDescriptor models required information about a project.
 type ProjectDescriptor struct {
-	Image              string `json:"buildImage"`
-	RepoManager        string `json:"repoManager"`
-	RepoURL            string `json:"repoUrl"`
-	RepoDescription    string `json:"repoDescription"`
+	// Image is the container image the associated project should be built in.
+	Image string `json:"buildImage"`
+	// RepoManager is the source code management system the project source is contained in.
+	RepoManager string `json:"repoManager"`
+	// RepoURL is largely informational for the human managing this project.  It is currently unused by Decap.
+	RepoURL string `json:"repoUrl"`
+	// RepoDescription is a human readable description of this project.
+	RepoDescription string `json:"repoDescription"`
+	// ManagedRegexStr is a regular expression that defines which refs (branches and tags) is willing to build as a result of
+	// a post-commit hook.  Manual builds are not subject to this regex.
 	ManagedRefRegexStr string `json:"managedRefRegex"`
-	Regex              *regexp.Regexp
+	// The formal regex associated with ManagedRefRegexStr above.
+	Regex *regexp.Regexp
 }
 
+// IsRefManaged is used by Decap to determine if a build should be launched as a result of a post-commit hook on a given ref.
 func (d ProjectDescriptor) IsRefManaged(ref string) bool {
 	return d.Regex == nil || d.Regex.MatchString(ref)
 }
 
+// Builds is a collection of historical Build's.
 type Builds struct {
 	Meta
 	Builds []Build `json:"builds"`
 }
 
+// Build models the historical build information available to Decap in the backing storage system.
 type Build struct {
 	ID         string `json:"id"`
 	ProjectKey string `json:"projectKey"`
@@ -57,30 +74,36 @@ type Build struct {
 	UnixTime   uint64 `json:"startTime"`
 }
 
+// Teams models a collection of logical Team's.
 type Teams struct {
 	Meta
 	Teams []Team `json:"teams"`
 }
 
+// Team models the name of a group of developers or a project.
 type Team struct {
 	Name string `json:"name"`
 }
 
+// Refs models a collection of logical Ref's.
 type Refs struct {
 	Meta
 	Refs []Ref `json:"refs"`
 }
 
+// Ref models a git branch or tag.
 type Ref struct {
 	RefID string `json:"ref"`
 	Type  string `json:"type"`
 }
 
+// ShutdownState models whether the build queue is open or closed.
 type ShutdownState struct {
 	Meta
 	State string `json:"state"`
 }
 
+// UserBuildEvent models an abstract build, independent of the source code management system that backs it.
 type UserBuildEvent struct {
 	Team_    string         `json:"team"`
 	Project_ string         `json:"project"`
@@ -88,6 +111,8 @@ type UserBuildEvent struct {
 	Deferral locks.Deferral `json:"deferral"`
 }
 
+// Deferred models a deferred build.  Builds are deferred when they cannot get a lock on their backing branch.  Decap uses locks
+// to prevent concurrent builds of the same branch.
 type Deferred struct {
 	Meta
 	DeferredEvents []UserBuildEvent `json:"deferred"`
