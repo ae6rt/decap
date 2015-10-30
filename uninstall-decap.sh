91f570388c4e2361549c59c18e645200a4c1ac97
Mark Petrovic <mark.petrovic@xoom.com>
2015-10-30 08:20:15 -0700

    point api endpoint at cluster

---
+++ uninstall-decap.sh
@@ -0,0 +1,4 @@
+kubectl delete namespace decap-system
+kubectl delete namespace decap
+kubectl delete -f k8s-resources/decap.yaml
+
--- web/static/scripts/config.js
+++ web/static/scripts/config.js
@@ -1,1 +1,1 @@
-"use strict";angular.module("config",[]).constant("ENV",{name:"development",apiEndpoint:"http://localhost:9090/api/v1"});
+"use strict";angular.module("config",[]).constant("ENV",{name:"development",apiEndpoint:"http://localhost:8080/api/v1/proxy/namespaces/decap-system/services/decap/api/v1"});
