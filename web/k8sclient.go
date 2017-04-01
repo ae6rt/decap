28c4a1075e03138ba12afa931fc4dd513f4478a4
Mark Petrovic <mspetrovic@gmail.com>
2017-03-31 20:39:53 -0700

    [dynamo-locks] extend kubeconfig for out of cluster use

--- web/k8sclient.go
+++ web/k8sclient.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"os"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 
 	k8sv1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
-// NewKubernetesClient returns a new client for use inside the cluster.
-// TODO extend for use outside the cluster
+// NewKubernetesClient returns a new client.
 func NewKubernetesClient() (k8sv1.PodsGetter, error) {
-	config, err := rest.InClusterConfig()
+
+	var cfg *rest.Config
+	var err error
+
+	cfg, err = clientcmd.BuildConfigFromFlags("", os.Getenv("HOME")+"/.kube/config")
 	if err != nil {
-		return nil, err
+		cfg, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+
+	clientset, err := kubernetes.NewForConfig(cfg)
 	return clientset.CoreV1(), err
 }
