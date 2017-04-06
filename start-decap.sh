fa13235f03cd6ec746ee275f6b400a2269cd6d4f
Mark Petrovic <mspetrovic@gmail.com>
2017-04-06 07:12:39 -0700

    [develop] record the creation of the decap deployment

--- start-decap.sh
+++ start-decap.sh
@@ -3,7 +3,9 @@
 set -ux
 
 kubectl create -f k8s-resources/decap-namespaces.yaml
+
 for i in decap decap-system; do
 	kubectl --namespace=$i create -f k8s-resources/decap-secrets.yaml
 done
-kubectl create -f k8s-resources/decap.yaml 
+
+kubectl create -f k8s-resources/decap.yaml --record
