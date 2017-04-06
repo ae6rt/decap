2a058112a03c33cdd871cc3d6cb780e88532dcca
Mark Petrovic <mspetrovic@gmail.com>
2017-04-06 07:43:49 -0700

    [develop] add helper script to simulate a github push event

---
+++ web/testdata/github-push.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Simulate a push event from github.  This assumes decap listens on a nodePort 192.168.99.100:31000.
+
+cat <<EOF | curl -i -X POST -H"X-Github-Event: push" -H"Content-type: application/json" -d @- http://192.168.99.100:31000/hooks/github
+{
+    "ref": "refs/heads/master", 
+    "repository": {
+        "full_name": "ae6rt/dynamodb-lab", 
+        "id": 35129377, 
+        "name": "dynamodb-lab", 
+        "owner": {
+            "email": "ae6rt@users.noreply.github.com", 
+            "name": "ae6rt"
+        }
+    }
+}
+EOF
