eeed7e209eab80ca012acb19c68df480e6029f6b
Mark Petrovic <mspetrovic@gmail.com>
2017-04-11 07:08:38 -0700

    [develop] fix typo testdaa/dlog.sh

--- web/testdata/dlog.sh
+++ web/testdata/dlog.sh
@@ -2,4 +2,4 @@
 
 set -u
 
-kubectl --namespace=decap-system logs -f $(kubectl --no-headers --namespace=decap-system get pods | awk '{print $1}')" 
+kubectl --namespace=decap-system logs -f $(kubectl --no-headers --namespace=decap-system get pods | awk '{print $1}')
